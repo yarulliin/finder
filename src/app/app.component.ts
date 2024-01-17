df53bec26174ce6a98842cd3c357cee4ab10dcf3
Яруллин Рифат <yarullinr@terralink.ru>
2024-01-17 17:27:09 +0300

    basic setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'fa-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-  title = 'finder';
-}
+export class AppComponent { }
